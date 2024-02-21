01356f9541cce799361c7a3a560c94c7140793db
Nannita1403 <nannimagi@gmail.com>
2024-02-21 15:22:02 +0100

    ok

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-import "./style.css";
+import './style.css';
 
 // 10 - 5
 const arrayCartas = [
   {
     id: 1,
-    color: "red",
+    color: 'red'
   },
   {
     id: 2,
-    color: "orange",
+    color: 'orange'
   },
   {
     id: 3,
-    color: "blue",
+    color: 'blue'
   },
   {
     id: 4,
-    color: "pink",
+    color: 'pink'
   },
   {
     id: 5,
-    color: "black",
+    color: 'black'
   },
   {
     id: 6,
-    color: "red",
+    color: 'red'
   },
   {
     id: 7,
-    color: "orange",
+    color: 'orange'
   },
   {
     id: 8,
-    color: "blue",
+    color: 'blue'
   },
   {
     id: 9,
-    color: "pink",
+    color: 'pink'
   },
   {
     id: 10,
-    color: "black",
-  },
+    color: 'black'
+  }
 ];
 
 let contador = 0;
@@ -51,9 +51,9 @@ let puntuacion = 0;
 
 arrayCartas.sort(() => Math.random() - Math.random());
 
-const divApp = document.querySelector("#app");
+const divApp = document.querySelector('#app');
 
-const puntuacionHTML = document.createElement("h3");
+const puntuacionHTML = document.createElement('h3');
 
 puntuacionHTML.textContent = `puntuacion: ` + puntuacion;
 
@@ -64,12 +64,13 @@ const resetearValores = () => {
   carta1 = undefined;
   carta2 = undefined;
   console.log(puntuacion);
-}
+};
 
 const resetearCarta = (cartaGenerica) => {
-  cartaGenerica.nodoHTML.style.backgroundColor = "#4d0038";
-  cartaGenerica.nodoHTML.style.backgroundImage = "url(https://www.transparenttextures.com/patterns/crissxcross.png)";
-}
+  cartaGenerica.nodoHTML.style.backgroundColor = '#4d0038';
+  cartaGenerica.nodoHTML.style.backgroundImage =
+    'url(https://www.transparenttextures.com/patterns/crissxcross.png)';
+};
 
 const comprobar = () => {
   if (carta1.datosCarta.color === carta2.datosCarta.color) {
@@ -90,16 +91,16 @@ const seleccionar = (divCartaNodoHTML, datosDeCadaCarta) => {
   if (contador < 2) {
     // seleccionar una carta - sólo lo hago cuando contador sea menor que dos
     contador++;
-    console.log("cartas seleccionadas: " + contador);
+    console.log('cartas seleccionadas: ' + contador);
     divCartaNodoHTML.style.backgroundColor = datosDeCadaCarta.color;
-    divCartaNodoHTML.style.backgroundImage = "none";
+    divCartaNodoHTML.style.backgroundImage = 'none';
   }
 
   // cuando hemos seleccionado la primera
   if (contador === 1) {
     carta1 = {
       nodoHTML: divCartaNodoHTML,
-      datosCarta: datosDeCadaCarta,
+      datosCarta: datosDeCadaCarta
     };
     console.log(carta1);
   }
@@ -108,7 +109,7 @@ const seleccionar = (divCartaNodoHTML, datosDeCadaCarta) => {
   if (contador === 2) {
     carta2 = {
       nodoHTML: divCartaNodoHTML,
-      datosCarta: datosDeCadaCarta,
+      datosCarta: datosDeCadaCarta
     };
     console.log(carta2);
     comprobar();
@@ -116,14 +117,12 @@ const seleccionar = (divCartaNodoHTML, datosDeCadaCarta) => {
 };
 
 arrayCartas.forEach((datosDeCadaCarta) => {
-  const divCartaNodoHTML = document.createElement("div");
+  const divCartaNodoHTML = document.createElement('div');
 
-  divCartaNodoHTML.className = "carta";
+  divCartaNodoHTML.className = 'carta';
 
   // elijo su nombre "seleccionar" y su funcionalidad - necesitamos saber que carta ha seleccionado el usuario, por lo tanto necesito enviarle a mi función la carta seleccionada - necesitamos el color que tiene esa carta para desvelarlo
-  divCartaNodoHTML.addEventListener("click", () =>
-    seleccionar(divCartaNodoHTML, datosDeCadaCarta)
-  );
+  divCartaNodoHTML.addEventListener('click', () => seleccionar(divCartaNodoHTML, datosDeCadaCarta));
 
   divApp.append(divCartaNodoHTML);
 });
